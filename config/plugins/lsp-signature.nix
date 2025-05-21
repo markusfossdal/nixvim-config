c9c3df9db4306d6bd3c3fb3c366d4cc2412e540c
Markus Fossdal <51286688+markusfossdal@users.noreply.github.com>
2025-05-21 10:48:41 +0200

    fixed lsp-signature and added toggle key <leader>m

--- config/plugins/lsp-signature.nix
+++ config/plugins/lsp-signature.nix
@@ -1,7 +1,21 @@
 {
   plugins.lsp-signature = {
     enable = true;
-    floatingWindow = true;
-    fintEnable = false;
+    settings = {
+      floating_window = true;
+      floating_window_above_cur_line = true;
+      extra_trigger_chars = [
+        "("
+        ","
+      ];
+      hint_prefix = {
+        above = "↙ ";
+        below = "↖ ";
+        current = "← ";
+      };
+      padding = " ";
+      shadow_guibg = "#121315";
+      toggle_key = "<leader>m";
+    };
   };
 }
